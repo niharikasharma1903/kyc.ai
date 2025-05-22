2bf9bd62933091d758845392a82367ea97ae61e5
Niharika Sharma <niharika.sharma@aionos.ai>
2025-05-22 11:59:07 +0530

    Fix CORS to allow Netlfiy frontend part 3

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ app = FastAPI(title="Tavily Company Research API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://kyc-ai-frontend.netlify.app/"], 
+    allow_origins=["https://kyc-ai-frontend.netlify.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
